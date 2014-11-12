6613835b1749a0d3cec3467bad35c36b2ca6ca6d
Sanquer <Sanquer@Asus-Klem>
2014-11-12 17:02:50 +0100

    Ajout Main

---
+++ src/recyclapp/Main.java
@@ -0,0 +1,22 @@
+/*
+ * Recyclapp - Projet de session GLO-2004, A2014
+ * Equipe Engrenages
+ * Alexandre Poli * Clement Sanquier * Gabriel Grenon * Martin Boisvert
+ */
+
+package recyclapp;
+
+//import recyclapp.view.MainView;
+
+/**
+ *
+ * @author Sanquer
+ */
+public class Main {
+    
+    
+    public static void main(String[] args) {
+        recyclapp.view.MainView mainView = new recyclapp.view.MainView();
+        mainView.setVisible(true);
+    }
+}
