35fc362727004faad9ac6be22eed0204a31a65e7
Sanquer <Sanquer@132.203.164.35>
2014-11-20 13:07:11 +0100

    Grille

--- src/recyclapp/view/DiagramView.java
+++ src/recyclapp/view/DiagramView.java
@@ -8,10 +8,48 @@ package recyclapp.view;
 
 import javax.swing.JPanel;
 
+
 /**
  *
- * @author Martin Boisvert
+ * @author Martin Boisvert / Clement Sanquer
  */
-public class DiagramView extends JPanel {
-    
-}
+
+  import java.awt.Color;
+  import java.awt.Graphics;
+  import java.awt.Graphics2D;
+ 
+  public class DiagramView extends JPanel
+  {
+       
+      public int taille;
+      
+    public DiagramView()
+    {
+        taille = 25;
+    }
+ 
+    @Override
+    protected void paintComponent( Graphics g )
+    {
+        super.paintComponent(g); 
+        Graphics2D g2 = (Graphics2D) g;
+
+        g2.setColor(Color.white);
+        //On le dessine de sorte qu'il occupe toute la surface
+        g2.fillRect(0, 0, this.getWidth(), this.getHeight());
+        
+        g2.setColor(Color.GRAY);
+        g.drawRect(0, 0, this.getWidth(), this.getHeight());
+        
+        g2.setColor(Color.GRAY);
+        
+        
+        
+        int i ;
+        for(i=1;i<((Math.max(this.getHeight(),this.getWidth()))/this.taille)+1;i++)
+        {
+            g2.drawLine(i*this.taille, 0, i*this.taille, this.getHeight());
+            g2.drawLine(0, i*this.taille, this.getWidth(), i*this.taille);
+        }
+    }
+  }
